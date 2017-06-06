0b0146f76dfb090ec0f547ff69b247d909f9aaf0
Dario Renzetti <dario.rnz@gmail.com>
2017-06-06 15:50:40 +0200

    Created Category, CardView and init MainActivity

--- app/src/main/java/com/example/archimede/ecommerce2/MainActivity.java
+++ app/src/main/java/com/example/archimede/ecommerce2/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.archimede.ecommerce2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.archimede.ecommerce2.data.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rView = (RecyclerView)findViewById(R.id.objs_recycler_view);
+        rView.setHasFixedSize(true);
+
+        final List<Category> CategoryList = new ArrayList<>();
+        for (int i = 0; i < 21; i++) {
+            CategoryList.add(new Category("Title", "Image", "Description"));
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/example/archimede/ecommerce2/data/Category.java
@@ -0,0 +1,73 @@
+package com.example.archimede.ecommerce2.data;
+
+/**
+ * Created by archimede on 06/06/17.
+ */
+
+public class Category {
+    private String title;
+    private String image;
+    private String description;
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "title='" + title + '\'' +
+                ", image=" + image +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        if (title != null ? !title.equals(category.title) : category.title != null) return false;
+        if (image != null ? !image.equals(category.image) : category.image != null) return false;
+        return description != null ? description.equals(category.description) : category.description == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (image != null ? image.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    public String getTitle() {
+    
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category(String title, String image, String description) {
+    
+        this.title = title;
+        this.image = image;
+        this.description = description;
+    }
+}
